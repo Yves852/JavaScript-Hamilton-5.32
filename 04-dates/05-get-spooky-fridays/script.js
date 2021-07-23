2b8469f78c7b7e08589524b851b96998cf058327
SporoZoite <lisavanhooydonk@gmail.com>
2021-07-23 09:44:33 +0200

    add exercices get spooky friday, mini carousel and worst ui 3

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -9,8 +9,25 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+(function() {
+    document.getElementById("run").addEventListener("click", function() {
+      var year =  document.getElementById("year").value;
+      console.log(year);
+      for (var month=0; month<12;month++)
+      {
+          var date= new Date(year,month,13); //get the 13th of the month//
+          if(date.getDay()==5) //get fridays//
+          {
+              console.log(date);
+              console.log (date.toLocaleString('eng', {month: 'long'})); //traduire le chiffre du mois en nom
+              document.write(date.toLocaleString('eng', {month: 'long'})+"<br>"); //afficher les uns en dessous des autres
+          }
+      }
+
+     
+    });
+    
+
 
 })();
